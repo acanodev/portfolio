61c9090002f1e827349f9db8e7c3a75b3b371a2c
acanodev <aaroncanofdez@gmail.com>
2026-06-02 19:37:32 +0200

    Prisma adapter config

---
+++ backend/config/prisma.js
@@ -0,0 +1,16 @@
+const { PrismaClient } = require("@prisma/client");
+const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
+require("dotenv").config();
+
+const adapter = new PrismaMariaDb({
+  host: process.env.ADAPTER_HOST || "localhost", // your database host
+  user: process.env.ADAPTER_USER || "root", // your database username
+  password: "your_password", // your database password
+  database: "your_database", // optional, your database name
+});
+
+const prisma = new PrismaClient({ adapter });
+
+module.exports = {
+  prisma,
+};
