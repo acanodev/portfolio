31b1b8dddda430b03a0c1df0eba462bd33600bad
acanodev <aaroncanofdez@gmail.com>
2026-06-04 12:46:06 +0200

    typo error for ADAPTER_DATABASE env variable at prisma.js corrected

--- backend/config/prisma.js
+++ backend/config/prisma.js
@@ -8,7 +8,7 @@ const adapter = new PrismaMariaDb({
   host: process.env.ADAPTER_HOST || "localhost", // your database host
   user: process.env.ADAPTER_USER || "root", // your database username
   password: process.env.ADAPTER_PASSWORD, // your database password
-  database: process.env.ADAPTER_DATABSE, // optional, your database name
+  database: process.env.ADAPTER_DATABASE, // optional, your database name
 });
 
 const prisma = new PrismaClient({ adapter });
