9903c6037f3f29bb7a169a5993d8318756de87d7
acanodev <aaroncanofdez@gmail.com>
2026-06-04 11:59:09 +0200

    .env path corrected at prisma configuration

--- backend/config/prisma.js
+++ backend/config/prisma.js
@@ -1,6 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
 const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
-require("dotenv").config();
+require('dotenv').config({
+  path: require('path').resolve(__dirname, '../.env')
+});
 
 const adapter = new PrismaMariaDb({
   host: process.env.ADAPTER_HOST || "localhost", // your database host
