079203911cb84a0efa8f717f842e69684334b8b9
acanodev <aaroncanofdez@gmail.com>
2026-06-04 23:20:09 +0200

    Subject changed to optional. Allows null or undefined values.

--- backend/middlewares/validations/contactMessageSchema.js
+++ backend/middlewares/validations/contactMessageSchema.js
@@ -3,7 +3,7 @@ const { z } = require('zod');
 const contactMessageSchema = z.object({
     name: z.string().min(1),
     email: z.string().email(),
-    subject: z.string().min(5),
+    subject: z.string().optional().min(5),
     message: z.string().min(10).max(1500)
 });
 
