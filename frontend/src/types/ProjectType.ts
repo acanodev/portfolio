321da8a5c564627775da7a0e8950d6fe1f15695a
acanodev <aaroncanofdez@gmail.com>
2026-06-05 11:53:06 +0200

    ProjectType created

---
+++ frontend/src/types/ProjectType.ts
@@ -0,0 +1,12 @@
+export type ProjectType = {
+    id: number,
+    title: string,
+    description_en?: string,
+    description_es?: string,
+    thumbnail_url?: string,
+    github_url?: string,
+    demo_url?: string,
+    featured: boolean,
+    created_at: string,
+    updated_at: string
+}
