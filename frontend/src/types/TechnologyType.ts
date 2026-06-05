4ffc4a9865b278a2fc9c847dd173abcfb37b0491
acanodev <aaroncanofdez@gmail.com>
2026-06-05 11:50:46 +0200

    TechnologyType created

---
+++ frontend/src/types/TechnologyType.ts
@@ -0,0 +1,8 @@
+export type TechnologyType = {
+    id: number,
+    name: string,
+    category: "frontend" | "backend" | "cms" | "databases" | "devops_and_tools" | "systems" | "documentation_and_office",
+    image_url?: string,
+    created_at: string,
+    updated_at: string
+}
