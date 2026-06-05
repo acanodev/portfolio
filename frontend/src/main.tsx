61757c72015eb8723b6ec50334c8895e692b575e
acanodev <aaroncanofdez@gmail.com>
2026-06-05 14:35:33 +0200

    BrowserRouter imported to make React Router Dom work propperly and normalize.css added to give more consistence to page elements in different web browsers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 /*import './index.css'*/
-import App from './App.tsx'
+import './normalize.css'
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
