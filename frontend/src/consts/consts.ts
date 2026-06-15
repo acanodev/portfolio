52d6f7c3c0470249d863c39595a00259f1b56962
acanodev <aaroncanofdez@gmail.com>
2026-06-15 12:27:11 +0200

    New string const for About page and CSS and Bootstrap tweaks.

--- frontend/src/consts/consts.ts
+++ frontend/src/consts/consts.ts
@@ -1,4 +1,10 @@
 export const BASE_URL = "/api";
 
 export const ABOUT_TEXT =
-  "I'm not just interested in coding — I'm interested in how applications and systems actually work. From full stack development to Linux environments, I enjoy working close to the system, building projects that combine software, infrastructure and problem solving. I value understanding over memorizing, and efficiency over complexity. 🎯 Long term: Become a developer with strong knowledge about both development and systems. 💡 I enjoy learning by building, experimenting and constantly improving my workflow.";
+  "I'm not just interested in coding — I'm interested in how applications and systems actually work. From full stack development to Linux environments, I enjoy working close to the system, building projects that combine software, infrastructure and problem solving. I value understanding over memorizing, and efficiency over complexity.";
+
+export const ABOUT_LONG_TERM =
+  "Become a developer with strong knowledge about both development and systems.";
+
+export const ABOUT_ENJOY =
+  "💡 I enjoy learning by building, experimenting and constantly improving my workflow and my programming skills with my current or new stack.";
--- frontend/src/views/About/About.tsx
+++ frontend/src/views/About/About.tsx
@@ -1,5 +1,5 @@
 import "./About.css";
-import { ABOUT_TEXT } from "../../consts/consts";
+import { ABOUT_ENJOY, ABOUT_LONG_TERM, ABOUT_TEXT } from "../../consts/consts";
 import { Helmet } from "react-helmet-async";
 
 export default function About() {
@@ -15,15 +15,29 @@ export default function About() {
         <h1 className="text-info display-3">About</h1>
       </header>
 
-      <div className="about-container pb-sm-5 pb-md-0 pb-lg-0">
+      <div className="about-container pb-sm-5 pb-md-5 pb-lg-5">
         <div className="row d-flex flex-sm-column flex-md-row flex-lg-row mt-4 mb-auto">
           <div className="col">
             <div className="placeholder about-placeholder bg-info w-100"></div>
           </div>
           <div className="col">
-            <p className="about-p">{ABOUT_TEXT}</p>
+            <div className="pt-sm-5 pt-md-0 pt-lg-0">
+              <p className="about-p">{ABOUT_TEXT}</p>
+              <p className="about-p">
+                <strong>🎯 Long term:</strong> {ABOUT_LONG_TERM}
+              </p>
+              <p className="about-p">{ABOUT_ENJOY}</p>
+            </div>
           </div>
         </div>
+      </div>
+
+      <header className="pt-0 text-center">
+        <h1 className="text-info display-3">Random facts</h1>
+      </header>
+
+      <div className="about-container random-facts-container pb-sm-5 pb-md-5 pb-lg-5">
+        <h3 className="text-info pt-2">🚧 Feature in development</h3>
       </div>
     </>
   );
