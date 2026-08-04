4934dc2bf2a798d5277710c017bb28ca15456481
acanodev <aaroncanofdez@gmail.com>
2026-08-04 20:10:21 +0200

    Added my formation to ExperienceSeeders.

--- backend/seeders/ExperienceSeeders.js
+++ backend/seeders/ExperienceSeeders.js
@@ -2,14 +2,22 @@ const { prisma } = require("../config/prisma");
 
 const experienceData = [
   {
-    company_name: "Escola Lledoner",
+    place: "Institut Carles Vallbona",
+    formation_es: "FPGM Sistemas Microinformáticos y redes (SMR)",
+    formation_en: "Intermediate Degree in Microcomputer Systems and Networks",
+    start_year: 2022,
+    end_year: 2024,
+    type: "education",
+    image_url: "/logo_vallbona.png"
+  },
+  {
+    place: "Escola Lledoner",
     job_title_en: "Computer Technician",
     job_title_es: "Técnico informático",
     description_en:
       "As a computer technician, I maintain and support computers, operating systems, and networks. I configure, install, and update operating systems and office suite on desktop computers and servers. I manage and maintain internal networks, solving the main possible issues. I document hardware and software configuration to ensure proper resource management. I configure and update systems and servers, providing support to the user against any issue.",
     description_es:
       "Como técnico informático me encargo del mantenimiento y soporte técnico de sistemas, redes y equipos informáticos. Realizo la configuración, instalación y actualización de sistemas operativos y software ofimático en ordenadores de escritorio y servidores. Administro y mantengo las redes internas, resolviendo posibles incidencias. Documento la configuración de hardware y software para garantizar una correcta gestión de recursos. Me encargo de la configuración y actualización de sistemas y servidores, prestando asistencia al usuario final ante cualquier problema.",
-    year: 2023,
     start_year: 2023,
     end_year: 2024,
     technologies: [
@@ -18,17 +26,27 @@ const experienceData = [
         "Word",
         "Excel",
         "SharePoint",
-    ]
+    ],
+    type: "job",
+    image_url: "https://agora.xtec.cat/ceiplledoner/wp-content/uploads/usu1347/2016/06/logoescola-1.jpg"
   },
   {
-    company_name: "Bujaldon Santiago SL",
+    place: "Institut Carles Vallbona",
+    formation_es: "FPGM Sistemas Microinformáticos y redes (SMR)",
+    formation_en: "Intermediate Degree in Microcomputer Systems and Networks",
+    start_year: 2024,
+    end_year: 2026,
+    type: "education",
+    image_url: "/logo_vallbona.png"
+  },
+  {
+    place: "Bujaldon Santiago SL",
     job_title_en: "Full Stack Developer",
     job_title_es: "Desarrollador Web Full Stack",
     description_en:
       "At Bujaldon Santiago, I managed databases to ensure accessibility and visualize data programming on the server side with PHP (specifically with Laravel), built the app using HTML, CSS, and Bootstrap, coded on the client side using JavaScript, and managed versions with Git, GitHub, and GitLab.",
     description_es:
       "En Bujaldon Santiago me dediqué a gestionar sistemas de bases de datos, realizar operaciones para garantizar la accesibilidad y visualización de los datos programando en entorno servidor con PHP (concretamente usando como framework Laravel), maquetar la aplicación web utilizando lenguajes de marcas HTML, estilos CSS con Bootstrap, programar en entorno cliente con JavaScript y gestionar las versiones con Git, GitHub y GitLab.",
-    year: 2025,
     start_year: 2025,
     end_year: 2025,
     technologies: [
@@ -46,7 +64,9 @@ const experienceData = [
         "Word",
         "Excel",
         "SharePoint"
-    ]
+    ],
+    type: "job",
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm11Gq59Ah2z4dLISev658iObgXM3T0AYFYQldNcYjE_F8dYQPsYDtMYw&s=10"
   },
 ];
 
